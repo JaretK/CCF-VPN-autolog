443f358336ea02d08d553e2eac8e570bccbc2b51
Jaret Karnuta <JaretK@users.noreply.github.com>
2017-12-22 10:06:46 -0500

    Update cisco.bat

--- cisco.bat
+++ cisco.bat
@@ -5,7 +5,7 @@ for /f "tokens=4" %%g in ('vpncli.exe state ^| FIND "state:"') do set vpnstate=%
 taskkill /F /IM vpnui.exe
 if %vpnstate%==Disconnected (
 	set /P case="Press (y) to access vpn.case.edu / press (n) to cancel:"
-	REM #batch is f**king stupid, so heres some botched logical or code
+	REM #batch is stupid, so heres some botched logical or code
 	if /i !case!==y (
 		vpncli.exe -s < case_cisco_profile.txt
 		start vpnui.exe
